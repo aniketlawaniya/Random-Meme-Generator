50d44db5078d58d94061aa879a2f632468925cc4
Aniket Lawaniya <alawaniya3@gmail.com>
2023-02-07 04:33:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header"
+import Form from "./components/Form"
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Form />
+        </div>
+    )
+}
